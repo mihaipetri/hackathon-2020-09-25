78c2e268ec04d54bcbe902982fcf0fd2f93d30bf
mihaipetri <mihaipetri@gmail.com>
2020-10-05 09:50:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200);
-  res.write('<h1>Cloud Provider: ' + (process.env.PORT || 'Not Set') + '</h1>');
+  res.write('<h1>Cloud Provider: ' + (process.env.CLOUD || 'Not Set') + '</h1>');
   res.write('<h1>Welcome to Hachathon 2020-09-25</h1>');
   res.end('<h1>Version: V1</h1>');
 });
